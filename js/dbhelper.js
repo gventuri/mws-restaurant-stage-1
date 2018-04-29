c214ef1fc75897e9c93aa9f961d60807873d6edf
Gabriele Venturi <gabri@MBP-di-Gabriele.homenet.telecomitalia.it>
2018-04-29 18:48:33 +0200

    Added support for single restaurant api

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -32,18 +32,15 @@ class DBHelper {
    * Fetch a restaurant by its ID.
    */
   static fetchRestaurantById(id, callback) {
-    // fetch all restaurants with proper error handling.
-    DBHelper.fetchRestaurants((error, restaurants) => {
-      if (error) {
+    fetch(DBHelper.DATABASE_URL+"/"+id).then(function(res){
+      if (res.status === 200) { // Got a success response from server!
+        return res.json();
+      } else { // Oops!. Got an error from server.
         callback(error, null);
-      } else {
-        const restaurant = restaurants.find(r => r.id == id);
-        if (restaurant) { // Got the restaurant
-          callback(null, restaurant);
-        } else { // Restaurant does not exist in the database
-          callback('Restaurant does not exist', null);
-        }
       }
+    }).then(function(res){
+      const restaurant = res;
+      callback(null, restaurant);
     });
   }
 
